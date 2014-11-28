b56d85b4c6e60bd558750b64732a0d8c0beb0ccc
luke <luke@sneakydigital.com>
2014-11-28 22:35:57 +1100

    ch1

--- bw_startup.py
+++ bw_startup.py
@@ -7,25 +7,16 @@ from decimal import Decimal
 import subprocess
 
 
+#Enabling QoS
 
-
-#this is initial API
-
-@route('/uniwide_sdn/usage/<name>/<port>/<device_id>/<maxrate>/<minrate>')
-def en(name,port,device_id,maxrate,minrate):
-	if port == 'em3':
-		sh1="sudo ~/usage/enqueue2.sh" + " " + str(name) + " " + str(device_id) + " " + str(maxrate) + " " + str(minrate)
-		print sh1
-		sh= "macid" + " " + str(device_id) + "  " + subprocess.check_output(sh1,shell=True)
-		sg=sh.split("  ")
-		a=" \n".join(sg)
-		return a
-	elif port == 'patch_cable_21':
-		sh7="sudo ~/usage/enqueue3.sh" + " " + str(name) + " " + str(device_id) + " " + str(maxrate) + " " + str(minrate)
-		s1="macid" + " " + str(device_id) + "  " + subproces.check_output(sh7,shell=True)
-		s2=s1.split("  ")
-		c=" \n".join(s2)
-		return c
+@route('/uniwide_sdn/usage/<name>/<device_id>/<maxrate>/<minrate>')
+def en(name,device_id,maxrate,minrate):
+	sh1="sudo ~/usage/enqueue2.sh" + " " + str(name) + " " + str(device_id) + " " + str(maxrate) + " " + str(minrate)
+	print sh1
+	sh= "macid" + " " + str(device_id) + "  " + subprocess.check_output(sh1,shell=True)
+	sg=sh.split("  ")
+	a=" \n".join(sg)
+	return a
 
 @route('/home_sdn/usage/<name>/<device_id>/<maxrate>/<minrate>')
 def home(name,device_id,maxrate,minrate):
@@ -37,28 +28,36 @@ def home(name,device_id,maxrate,minrate):
 	b=" \n".join(t2)
 	return b
 
+#rate limit control
+
 @route('/home_sdn/control/<queue_id>/<maxrate>')
 def home_ctrl(queue_id,maxrate):
 	sh3="sudo ~/usage/home.sh" + " " + str(queue_id) + " "+str(maxrate)
 	print sh3
 	subprocess.call(sh3,shell=True)
+	return " Rate limt is done"
 	
 @route('/uniwide_sdn/control/<queue_id>/<maxrate>')
 def uniwide_ctrl(queue_id,maxrate):
 	sh4="sudo ~/usage/ch_bw.sh" + " " + str(queue_id) + " " + str(maxrate)
 	subprocess.call(sh4,shell=True)
+	return "Rate limit is done "
+
+
+# disabling QoS
 
 @route('/home_sdn/delete/<qos_id>/<q_id>/<name>')
 def del_home(qos_id,q_id,name):
 	sh5="sudo ~/usage/del_home.sh" + " " + str(qos_id) + " " + str(q_id) + " " + str(name)
 	print sh5
-	subprocess.call(sh5,shell=True)
+	m=subprocess.check_output(sh5,shell=True)
+	return m
 
 @route('/uniwide_sdn/delete/<port>/qos_id>/<q_id>/<name>')
 def del_uni(port,qos_id,q_id,name):
 	sh6 ="sudo ~/usage/del_uniwide.sh" + " " + str(port) + " " + str(qos_id) + " " + str(q_id) +" " + str(name)
-	subprocess.call(sh6,shell=True)
-
+	n=subprocess.check_output(sh6,shell=True)
+	return n
 
 if __name__ == "__main__":
 
