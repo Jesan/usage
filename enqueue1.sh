5e5d39dc214e5f52346b5329b8ac23173a19ad1b
Jesan <jesanraj4u@gmail.com>
2014-12-02 15:37:43 +1100

    Update enqueue1.sh

--- enqueue1.sh
+++ enqueue1.sh
@@ -7,9 +7,9 @@ export maxrate=$4
 export minrate=$5
 
 
-sudo ovs-vsctl -- set port p1p2 qos=@newqos -- --id=@newqos create qos type=linux-htb  queues=$n=@q1 -- --id=@q1 create queue other-config:max-rate=$maxrate other-config:min-rate=$minrate
+sudo ovs-vsctl -- set port p1p2 qos=@newqos -- --id=@newqos create qos type=linux-htb  queues=$n=@q$n -- --id=@q$n create queue other-config:max-rate=$maxrate other-config:min-rate=$minrate
 
 
-curl -d '{"switch": "00:00:a0:36:9f:1e:0c:cd", "name":"'"$name"'", "priority":"10000","cookie":"0","dst-mac":"'"$device_id"'","active":"true", "actions":"enqueue=2:'"$n"'"}' http://149.171.37.183:8080/wm/staticflowentrypusher/json
+curl -d '{"switch": "00:00:a0:36:9f:1e:0c:cd", "name":"'"$name"'", "priority":"32768","cookie":"0","dst-mac":"'"$device_id"'","active":"true", "actions":"enqueue=2:'"$n"'"}' http://149.171.37.183:8080/wm/staticflowentrypusher/json
 
 
